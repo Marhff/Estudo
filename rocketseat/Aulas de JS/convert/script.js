47f9a2c92238c39a2067c7b8bc1cbdddc91aa877
Marcos França <marcosfranca310430@gmail.com>
2025-06-10 15:55:15 -0300

    exclusao de letras

---
+++ rocketseat/Aulas de JS/convert/script.js
@@ -0,0 +1,7 @@
+ const amount = document.getElementById("amount");
+
+ amount.addEventListener("input", () => {
+  const hasCharacterRegex = /\D+/g
+  amount.value = amount.value. replace(hasCharacterRegex, "");
+
+ });
