17f8aedb463666743437b813295c4a86b81f006e
Marcos França <marcosfranca310430@gmail.com>
2025-06-10 20:24:07 -0300

    Finish

--- rocketseat/Aulas de JS/convert/script.js
+++ rocketseat/Aulas de JS/convert/script.js
@@ -1,7 +1,43 @@
- const amount = document.getElementById("amount");
+const USD = 5.54
+const EUR = 6.35
+const GBP = 7.52
+
+
+const form = document.querySelector("form")
+const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
 
  amount.addEventListener("input", () => {
   const hasCharacterRegex = /\D+/g
   amount.value = amount.value. replace(hasCharacterRegex, "");
+ })
 
- });
+ form.onsubmit = (event) => {
+  event.preventDefault()
+  switch (currency.value){
+    case "USD":
+      convertCurrency(amount.value,USD,"US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value,EUR,"€")
+      break
+    case "GBP":
+      convertCurrency(amount.value,GBP,"£")
+      break
+  }
+ }
+
+ function convertCurrency(amount, price,symbol){
+try {
+  description.textContent = `${symbol}1 = ${price}`
+
+  footer.classList.add("show-result")
+} catch (error) {
+  console.log (error)
+  footer.classList.remove("show-result") 
+}
+
+ }
+  
